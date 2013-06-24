d3967fb98ccccd591dd749738225a7c75fa62638
Ash Caire <ash.caire@catchoftheday.com.au>
2013-06-24 11:30:30 +1000

    add test for program param

--- spec/defines/windows_firewall/exception_spec.rb
+++ spec/defines/windows_firewall/exception_spec.rb
@@ -3,7 +3,7 @@ require 'spec_helper'
 describe 'windows_firewall::exception', :type => :define do
 
   ['Windows Server 2003','Windows Server 2003 R2','Windows XP'].each do |os|
-    context "with OS: #{os}, ensure: present" do
+    context "port rule with OS: #{os}, ensure: present" do
      let :facts do
        { :operatingsystemversion => os }
      end
@@ -23,7 +23,7 @@ describe 'windows_firewall::exception', :type => :define do
   end
 
   ['Windows Server 2012', 'Windows Server 2008','Windows Server 2008 R2','Windows 8','Windows 7','Windows Vista'].each do |os|
-    context "with OS: #{os}, ensure: present" do
+    context "port rule with OS: #{os}, ensure: present" do
      let :facts do
        { :operatingsystemversion => os }
      end
@@ -43,14 +43,54 @@ describe 'windows_firewall::exception', :type => :define do
   end
 
   ['Windows Server 2003','Windows Server 2003 R2','Windows XP'].each do |os|
-    context "with OS: #{os}, ensure: absent" do
+    context "program rule with OS: #{os}, ensure: present" do
+     let :facts do
+       { :operatingsystemversion => os }
+     end
+     let :title do 'Windows Remote Management' end
+     let :params do
+       { :ensure => 'present', :direction => 'in', :action => 'allow', :enabled => 'yes',
+         :protocol => 'TCP', :local_port => 'C:\foo.exe', :key_name => 'WINRM-HTTP-In-TCP',
+         :display_name => 'Windows Remote Management', :description => 'Inbound rule for WinRM'
+       }
+     end
+
+     it { should contain_exec('set rule Windows Remote Management').with(
+       'command' => 'C:\\Windows\\System32\\netsh.exe firewall add allowedprogram name="Windows Remote Management" mode=ENABLE program="C:\\foo.exe"',
+       'provider' => 'windows'
+     ) }
+    end
+  end
+
+  ['Windows Server 2012', 'Windows Server 2008','Windows Server 2008 R2','Windows 8','Windows 7','Windows Vista'].each do |os|
+    context "program rule with OS: #{os}, ensure: present" do
+     let :facts do
+       { :operatingsystemversion => os }
+     end
+     let :title do 'Windows Remote Management' end
+     let :params do
+       { :ensure => 'present', :direction => 'in', :action => 'allow', :enabled => 'yes',
+         :protocol => 'TCP', :local_port => '5985', :key_name => 'WINRM-HTTP-In-TCP',
+         :display_name => 'Windows Remote Management', :description => 'Inbound rule for WinRM'
+       }
+     end
+
+     it { should contain_exec('set rule Windows Remote Management').with(
+       'command' => 'C:\\Windows\\System32\\netsh.exe advfirewall firewall add rule name="Windows Remote Management" description="Inbound rule for WinRM" dir=in action=allow enable=yes program="C:\\foo.exe"',
+       'provider' => 'windows'
+     ) }
+    end
+  end
+
+  ['Windows Server 2003','Windows Server 2003 R2','Windows XP'].each do |os|
+    context "port rule with OS: #{os}, ensure: absent" do
      let :facts do
        { :operatingsystemversion => os }
      end
      let :title do 'Windows Remote Management' end
      let :params do
        { :ensure => 'absent', :direction => 'in', :action => 'allow', :enabled => 'yes',
-         :protocol => 'TCP', :local_port => '5985', :key_name => 'WINRM-HTTP-In-TCP', 
+         :protocol => 'TCP', :local_port => '5985', :key_name => 'WINRM-HTTP-In-TCP',
          :display_name => 'Windows Remote Management', :description => 'Inbound rule for WinRM'
        }
      end
@@ -63,7 +103,7 @@ describe 'windows_firewall::exception', :type => :define do
   end
 
   ['Windows Server 2012', 'Windows Server 2008','Windows Server 2008 R2','Windows 8','Windows 7','Windows Vista'].each do |os|
-    context "with OS: #{os}, ensure: absent" do
+    context "port rule with OS: #{os}, ensure: absent" do
       let :facts do
         { :operatingsystemversion => os }
       end
@@ -77,6 +117,46 @@ describe 'windows_firewall::exception', :type => :define do
 
       it { should contain_exec('set rule Windows Remote Management').with(
         'command' => 'C:\\Windows\\System32\\netsh.exe advfirewall firewall delete rule name="Windows Remote Management" description="Inbound rule for WinRM" dir=in action=allow enable=yes protocol=TCP localport=5985',
+        'provider' => 'windows'
+      ) }
+    end
+  end
+
+  ['Windows Server 2003','Windows Server 2003 R2','Windows XP'].each do |os|
+    context "program rule with OS: #{os}, ensure: absent" do
+     let :facts do
+       { :operatingsystemversion => os }
+     end
+     let :title do 'Windows Remote Management' end
+     let :params do
+       { :ensure => 'absent', :direction => 'in', :action => 'allow', :enabled => 'yes',
+         :protocol => 'TCP', :local_port => '5985', :key_name => 'WINRM-HTTP-In-TCP',
+         :display_name => 'Windows Remote Management', :description => 'Inbound rule for WinRM'
+       }
+     end
+
+     it { should contain_exec('set rule Windows Remote Management').with(
+       'command' => 'C:\\Windows\\System32\\netsh.exe firewall delete portopening name="Windows Remote Management" mode=ENABLE program="C:\\foo.exe"',
+       'provider' => 'windows'
+     ) }
+    end
+  end
+
+  ['Windows Server 2012', 'Windows Server 2008','Windows Server 2008 R2','Windows 8','Windows 7','Windows Vista'].each do |os|
+    context "program rule with OS: #{os}, ensure: absent" do
+      let :facts do
+        { :operatingsystemversion => os }
+      end
+      let :title do 'Windows Remote Management' end
+      let :params do
+        { :ensure => 'absent', :direction => 'in', :action => 'allow', :enabled => 'yes',
+          :protocol => 'TCP', :local_port => '5985', :key_name => 'WINRM-HTTP-In-TCP',
+          :display_name => 'Windows Remote Management', :description => 'Inbound rule for WinRM'
+        }
+      end
+
+      it { should contain_exec('set rule Windows Remote Management').with(
+        'command' => 'C:\\Windows\\System32\\netsh.exe advfirewall firewall delete rule name="Windows Remote Management" description="Inbound rule for WinRM" dir=in action=allow enable=yes program="C:\\foo.exe"',
         'provider' => 'windows'
       ) }
     end
