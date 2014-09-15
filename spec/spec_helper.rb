13f62dcb28fdff1728d374c931d587927f612b34
liamjbennett <liam.j.bennett@gmail.com>
2014-09-15 11:36:19 +0100

    tweaking fixures

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -2,5 +2,3 @@ require 'rubygems'
 require 'puppetlabs_spec_helper/module_spec_helper'
 
 $:.unshift File.join(File.dirname(__FILE__),  'fixtures', 'modules', 'stdlib', 'lib')
-$:.unshift File.join(File.dirname(__FILE__),  'fixtures', 'modules', 'altlib', 'lib')
-$:.unshift File.join(File.dirname(__FILE__),  'fixtures', 'modules', 'augeasproviders', 'lib')
